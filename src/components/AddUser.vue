fb5c6ee9c583e1165fc158c0b24bde5a9a0e98b4
FelipeeM <FelipeeM@users.noreply.github.com>
2022-04-21 20:34:59 -0300

    New project

---
+++ src/components/AddUser.vue
@@ -0,0 +1,30 @@
+<template>
+    <p>Funcionou</p>
+</template>
+<script>
+
+
+export default {
+    name:"add-user"
+}
+</script>
+
+<style scoped>
+#nav{
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    
+}
+#button-nav{
+    justify-content: right;
+
+}
+#titulo{
+    color: rgb(255, 255, 255);
+    justify-content: left;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id ="nav">     
+            <h2 id ="titulo">Interface Vue com CRUD</h2>
+        <div id ="button-nav"> 
+            <router-link  to="/">Home</router-link> |
+            <router-link  to="/about">About</router-link> |
+
+            <router-link  to="/add-user">Adicionar usuario</router-link>
+        </div>
+    </div>
+     <router-view/>
+</template>
+<script>
+
+
+export default {
+    name:"Navebar"
+}
+</script>
+
+<style scoped>
+#nav{
+    background-color: rgb(241, 110, 110);
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    
+}
+#button-nav{
+    justify-content: right;
+
+}
+#titulo{
+    color: rgb(255, 255, 255);
+    justify-content: left;
+}
+</style>
